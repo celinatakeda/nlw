0f53c93ff12a1147108feac385f9176d10775983
Celina Takeda <82118386+celinatakeda@users.noreply.github.com>
2022-09-13 10:18:48 -0300

    commit

--- edicao9/esports/web/tailwind.config.cjs
+++ edicao9/esports/web/tailwind.config.cjs
@@ -8,7 +8,8 @@ module.exports = {
     extend: {      
       backgroundImage:{
         galaxy: "url('/background-galaxy.png')",
-        'nwl-gradient':'linear-gradient(89.86deg, #9572FC 23.88%, #43E7AD 33.94%, #E1D55D 44.57%)',
+        'nwl-gradient':'linear-gradient(90deg, #9572FC 0%, #43E7AD 50.00%, #E1D55D 100%)',
+        'game-gradient': 'linear-gradient(180deg, rgba(0,0,0,0) 0%, rgba(0,0,0,0.9) 67.88%)',
       }
     },
   },
