f30790b74bbf7a5dc72dc88e2316f6e8bd99cc85
Celina Takeda <82118386+celinatakeda@users.noreply.github.com>
2022-09-14 10:32:25 -0300

    commit

--- edicao9/esports/server/src/server.ts
+++ edicao9/esports/server/src/server.ts
@@ -1,13 +1,27 @@
 //const express = require('express');
 //Usando o module substituindo o de cima
 import express from 'express'
+import { PrismaClient } from '@prisma/client'
 
 const app = express()
+const prisma = new PrismaClient({
+  log: ['query']
+})
 
 // HTTP methods / API RESTful / HTTP Codes
 
-app.get('/games', (request, response) => {
-  return response.json([]);
+app.get('/games', async (request, response) => {
+  const games = await prisma.game.findMany({
+    include: {
+      _count: {
+        select: {
+          ads: true,
+        }
+      }
+    }
+  })
+
+  return response.json([games]);
 })
 
 app.post('/ads', (request, response) => {
@@ -15,18 +29,18 @@ app.post('/ads', (request, response) => {
 })
 
 // concatenação de recursos
-app.get('/games/:id/ads', (request, response) => {
-  //const gameId = request.params.id;
+app.get('/games/:id/ads', async (request, response) => {
+  const gameId = request.params.id;
 
   //return response.send(gameId);
 
-  return response.json([
-    {id: 1, name: 'Anúncio 1'},
-    {id: 2, name: 'Anúncio 2'},
-    {id: 3, name: 'Anúncio 3'},
-    {id: 4, name: 'Anúncio 4'},
-    {id: 5, name: 'Anúncio 5'},
-  ])
+  const ads = await prisma.ad.findMany({
+    where: {
+      gameId,
+    }
+  })
+
+  return response.json([ads])
 })
 
 app.get('/ads/:id/discord', (request, response) => {
