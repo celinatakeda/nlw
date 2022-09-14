0abf77f28ad65be80ddc85d07b6bddd7269e606a
Celina Takeda <82118386+celinatakeda@users.noreply.github.com>
2022-09-14 08:44:13 -0300

    commit

--- edicao9/esports/server/src/server.ts
+++ edicao9/esports/server/src/server.ts
@@ -4,13 +4,35 @@ import express from 'express'
 
 const app = express()
 
-app.get('/ads', (request, response) => {
+// HTTP methods / API RESTful / HTTP Codes
+
+app.get('/games', (request, response) => {
+  return response.json([]);
+})
+
+app.post('/ads', (request, response) => {
+  return response.status(201).json([]);
+})
+
+// concatenação de recursos
+app.get('/games/:id/ads', (request, response) => {
+  //const gameId = request.params.id;
+
+  //return response.send(gameId);
+
   return response.json([
     {id: 1, name: 'Anúncio 1'},
     {id: 2, name: 'Anúncio 2'},
     {id: 3, name: 'Anúncio 3'},
     {id: 4, name: 'Anúncio 4'},
+    {id: 5, name: 'Anúncio 5'},
   ])
+})
+
+app.get('/ads/:id/discord', (request, response) => {
+  //const adId = request.params.id;
+
+  return response.json([])
 })
 
 app.listen(3333)
