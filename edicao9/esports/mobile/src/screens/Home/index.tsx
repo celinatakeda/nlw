c6452fd80500be1ba9f180f2d2ca1bd84ebfe92c
Celina Takeda <82118386+celinatakeda@users.noreply.github.com>
2022-09-16 07:51:46 -0300

    commit

--- edicao9/esports/mobile/src/screens/Home/index.tsx
+++ edicao9/esports/mobile/src/screens/Home/index.tsx
@@ -1,6 +1,6 @@
 import {useEffect, useState} from 'react';
-import { View, Image, FlatList } from 'react-native';
-
+import { Image, FlatList } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import logoImg from '../../assets/logo-nlw-esports.png';
 
 import { GameCard, GameCardProps } from '../../components/GameCard';
@@ -18,7 +18,7 @@ export function Home() {
   , []});
 
   return (
-    <View style={styles.container}>
+    <SafeAreaView  style={styles.container}>
       <Image
         source={logoImg}
         style={styles.logo}
@@ -41,6 +41,6 @@ export function Home() {
         horizontal
         contentContainerStyle={styles.contentList}
       />
-    </View>
+    </SafeAreaView>
   );
 }
