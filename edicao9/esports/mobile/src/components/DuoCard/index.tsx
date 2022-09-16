921fcf2b9af3134a2d9273697cf01f33adc53925
Celina Takeda <82118386+celinatakeda@users.noreply.github.com>
2022-09-16 10:37:52 -0300

    commit

--- edicao9/esports/mobile/src/components/DuoCard/index.tsx
+++ edicao9/esports/mobile/src/components/DuoCard/index.tsx
@@ -1,30 +1,45 @@
 import { View } from 'react-native';
+import { THEME } from '../../theme';
 import { DuoInfo } from '../DuoInfo';
 
 import { styles } from './styles';
 
-export function DuoCard() {
+export interface DuoCardProps {
+  id: string;
+  hourEnd: string;
+  hourStart: string;
+  name: string;
+  useVoiceChannel: boolean;
+  weekDays: string[];
+  yearsPlaying: number;
+}
+
+interface Props {
+  data: DuoCardProps;
+}
+
+export function DuoCard({ data }: Props) {
   return (
     <View style={styles.container}>
       <DuoInfo
         label="Nome"
-        value="Diego Fernandes"
+        value={data.name}
       />
 
       <DuoInfo
-        label="Nome"
-        value="Diego Fernandes"
+        label="Tempo de jogo"
+        value={`${data.yearsPlaying} anos`}
       />
 
       <DuoInfo
-        label="Nome"
-        value="Diego Fernandes"
+        label="Disponibilidade"
+        value={`${data.weekDays.length} dias \u2022 ${data.hourStart} ${data.hourEnd}`}
       />
 
       <DuoInfo
-        label="Nome"
-        value="Diego Fernandes"
-        colorValue="#984"
+        label="Chamada de áudio"
+        value={data.useVoiceChannel ? "Sim" : "Não"}
+        colorValue={data.useVoiceChannel ? THEME.COLORS.SUCCESS : THEME.COLORS.ALERT}
       />
     </View>
   );
