9a22af750197446f215f51679bd60c29d3bb064e
Celina Takeda <82118386+celinatakeda@users.noreply.github.com>
2022-09-16 09:56:36 -0300

    commit

---
+++ edicao9/esports/mobile/src/components/DuoCard/index.tsx
@@ -0,0 +1,31 @@
+import { View } from 'react-native';
+import { DuoInfo } from '../DuoInfo';
+
+import { styles } from './styles';
+
+export function DuoCard() {
+  return (
+    <View style={styles.container}>
+      <DuoInfo
+        label="Nome"
+        value="Diego Fernandes"
+      />
+
+      <DuoInfo
+        label="Nome"
+        value="Diego Fernandes"
+      />
+
+      <DuoInfo
+        label="Nome"
+        value="Diego Fernandes"
+      />
+
+      <DuoInfo
+        label="Nome"
+        value="Diego Fernandes"
+        colorValue="#984"
+      />
+    </View>
+  );
+}
